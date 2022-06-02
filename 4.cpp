34b2d869e274335442dfa222fa7427f064e7609b
ni511 <103868519+ni511@users.noreply.github.com>
2022-06-02 23:09:21 +0530

    Update 4.cpp

--- 4.cpp
+++ 4.cpp
@@ -1,4 +1,8 @@
-//to print alphabets pattern
+/*to print alphabets pattern
+n=3
+AAA
+BBB
+CCC*/
 // Online C++ compiler to run C++ program online
 #include <iostream>
 using namespace std;
@@ -14,6 +18,94 @@ int main()
         {
             char ch='A'+i-1;
             cout<<ch;
+            j++;
+        }
+        cout<<endl;
+        i++;
+    }
+}
+/*TOO PRINT OUTPUT LIKE
+N:
+3
+ABC
+ABC
+ABC
+*/
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+int main()
+{
+    int i=1,n;
+    cout<<"N:\n";
+    cin>>n;
+    while(i<=n)
+    {
+        int j=1;
+        while(j<n)
+        {
+            char ch='A'+j-1;
+            cout<<ch;
+            j++;
+        }
+        cout<<endl;
+        i++;
+    }
+}
+/*TOO PRINT PATTERN LIKE
+N:
+3
+ABC
+DEF
+GHI
+*/
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+int main()
+{
+    int i=1,n,count=1;
+    cout<<"N:\n";
+    cin>>n;
+    while(i<=n)
+    {
+        int j=1;
+        while(j<=n)
+        {
+            char ch='A'+count-1;
+            cout<<ch;
+            count++;
+            j++;
+        }
+        cout<<endl;
+        i++;
+    }
+}
+/*TOO GET OUTPUT LIKE
+N:
+3
+ABC
+BCD
+CDE
+*/
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+int main()
+{
+    int i=1,n;
+    cout<<"N:\n";
+    cin>>n;
+    while(i<=n)
+    {
+        int j=1;
+        int count=0;
+        while(j<=n)
+        {
+            
+            char ch='A'+i-1+count;
+            cout<<ch;
+            count++;
             j++;
         }
         cout<<endl;
